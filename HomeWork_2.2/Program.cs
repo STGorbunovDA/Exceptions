83d6e4240ff6854adf55130bcb67604e04172849
STGorbunovDA <bous07@mail.ru>
2023-08-29 06:45:42 +0300

    Add HomeWork_2.2

---
+++ HomeWork_2.2/Program.cs
@@ -0,0 +1,10 @@
+﻿try
+{
+    double d = 0; //Исправлено объявление переменной d на double, чтобы избежать деления на 0.
+    double catchedRes1 = intArray[8] / d;
+    Console.WriteLine("catchedRes1 = " + catchedRes1);
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("Catching exception: " + e);
+}
