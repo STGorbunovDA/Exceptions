49dc6ffb40b26a2cad4b6d120802cd0eae82ff51
STGorbunovDA <bous07@mail.ru>
2023-08-29 06:39:51 +0300

    Add HomeWork_2.1

---
+++ HomeWork_2.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float number = ReadFloatFromUser();
+        Console.WriteLine("Введенное число: " + number);
+    }
+
+    static float ReadFloatFromUser()
+    {
+        string input;
+        float number;
+        bool success = false;
+
+        do
+        {
+            Console.Write("Введите дробное число: ");
+            input = Console.ReadLine();
+
+            success = float.TryParse(input, out number);
+
+            if (!success)
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+            }
+        }
+        while (!success);
+
+        return number;
+    }
+}
