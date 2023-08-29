2c92ab14ea8a650dae8cc9a6a227c2ab8f0a2953
STGorbunovDA <bous07@mail.ru>
2023-08-29 06:57:44 +0300

    Add HomeWork_2.4

--- HomeWork_2.3/Program.cs
+++ HomeWork_2.3/Program.cs
@@ -2,7 +2,7 @@
 
 public static void main(String[] args) throws FileNotFoundException
 {
-try {
+    try {
         int a = 90;
         int b = 3;
         System.out.println(a / b);
@@ -27,6 +27,42 @@ catch (Exception ex) {
 public static void printSum(Integer a, Integer b) throws FileNotFoundException
 {
     System.out.println(a + b);
+}
+
+#endregion
+
+#region С#
+
+try
+{
+    int a = 90;
+    int b = 3;
+    Console.WriteLine(a / b);
+    PrintSum(23, 234);
+    int[] abc = { 1, 2 };
+    abc[3] = 9;
+}
+catch (NullReferenceException ex)
+{
+    Console.WriteLine("Указатель не может указывать на null!");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Ошибка при делении на ноль!");
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Индекс массива выходит за пределы его размера!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Что-то пошло не так...");
+}
+
+
+static void PrintSum(int? a, int? b)
+{
+    Console.WriteLine(a + b);
 }
 
 #endregion
---
+++ HomeWork_2.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Введите строку: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                    throw new Exception("Пустые строки вводить нельзя");
+
+                Console.WriteLine("Вы ввели: " + input);
+                break; // если введена непустая строка, выходим из цикла
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
